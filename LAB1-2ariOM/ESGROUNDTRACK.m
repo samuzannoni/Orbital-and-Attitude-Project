0bc3109a9250989bc34d0e7ad53c658dd0e61dd1
ariluglii <116026173+ariluglii@users.noreply.github.com>
2022-10-21 17:33:41 +0200

    Update ESGROUNDTRACK.m

--- LAB1-2ariOM/ESGROUNDTRACK.m
+++ LAB1-2ariOM/ESGROUNDTRACK.m
@@ -18,26 +18,26 @@ n_orb = 30;
 r0 =[ 3108.128; -1040.299; -6090.022];
 v0 = [5.743; 8.055; 1.555];
 
-[alpha, delta, lon, lat] = groundtrack(r0,v0,th_g0,n_orb, 1);
+[alpha, delta, lon, lat] = groundtrack(r0,v0,th_g0,n_orb);
 
 %% case 3.1
 n_orb= 5;
 r0 = [5493.312; 4609.436; 0.000]; 
 v0 = [-4.792; 5.711; 0.000];
 
-[alpha, delta, lon, lat] = groundtrack(r0,v0,th_g0,n_orb, 1);
+[alpha, delta, lon, lat] = groundtrack(r0,v0,th_g0,n_orb);
 
 %% case 3.2
 r0 = [5493.312; 3991.889; 2304.718 ];
 v0 = [-4.792; 4.946; 2.856];
 
-[alpha, delta, lon, lat] = groundtrack(r0,v0,th_g0,n_orb, 1);
+[alpha, delta, lon, lat] = groundtrack(r0,v0,th_g0,n_orb);
 
- %% case 3.3
- r0 = [5493.312; -641.510; 4564.578];
- v0 = [-4.792; -0.795; 5.656];
+%% case 3.3
+r0 = [5493.312; -641.510; 4564.578];
+v0 = [-4.792; -0.795; 5.656];
   
-[alpha, delta, lon, lat] = groundtrack(r0,v0,th_g0,n_orb, 1);
+[alpha, delta, lon, lat] = groundtrack(r0,v0,th_g0,n_orb);
 
 
 

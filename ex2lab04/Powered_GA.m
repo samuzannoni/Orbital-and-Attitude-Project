4c426f0be7dbaf1c53820d67b5d2d71e9dd445b2
ariluglii <116026173+ariluglii@users.noreply.github.com>
2022-12-26 21:17:59 +0100

    Update Powered_GA.m

--- ex2lab04/Powered_GA.m
+++ ex2lab04/Powered_GA.m
@@ -1,13 +1,10 @@
-[rp] = = Powered_GA(v_inf_minus,v_inf_plus,turn_angle,rp0,mu);
-delta_minus  = @(rp) 2*asin(1./(1+ rp.*norm(v_inf_minus)^2/mu));
-delta_plus   = @(rp) 2*asin(1./(1+ rp.*norm(v_inf_plus)^2/mu));
-r_p_SolveFun = @(rp) (delta_minus(rp) + delta_plus(rp))/2 - delta;
-r_p_min = R_planet + h_atm;
+function [rp] = PGA(turn_angle,v_inf_minus,v_inf_plus,mu,rp0)
+vinfminus= norm(v_inf_minus);
+vinfplus= norm(v_inf_plus);
+
+turn_angle_solve = @(rp) asin(1./(1+ rp*vinfminus^2/mu))+asin(1./(1+ rp*vinfplus^2/mu))-turn_angle;
+
 options = optimoptions('fsolve','TolFun',1e-14,'Display','off');
 
-rp = fsolve(r_p_SolveFun, r_p_min, options);  % Periapsis radiu of the hyperbolas [km]
-
-
-% rp0 = astroConstants(23);
-% delta = @(rp) asin(1/(1+rp.*norm(v_inf_minus)^2/mu_E))+asin(1/(1+rp.*norm(v_inf_plus)^2/mu_E));
-% rp = fzero(delta,rp0);
+rp = fsolve(turn_angle_solve, rp0, options);
+end
